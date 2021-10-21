77ca72b5c471e45ea9247fdf1f20b8b8878f788d
Yevhendzyuba <evgendzuba7@gmail.com>
2021-10-21 16:51:18 +0300

    added task 1

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const example1 = document.querySelectorAll('li');
-console.log(example1[0].textContent, example1[4].textContent, example1[1].textContent, example1[3].textContent, example1[2].textContent);
+const example1 = document.getElementById('list');
+console.log(example1.children[0].innerText, example1.children[4].innerText, example1.children[1].innerText, example1.children[3].innerText, example1.children[2].innerText);
 
 
 const example2 = document.getElementsByTagName("li");
 console.log(example2[0].innerText, example2[4].innerText, example2[1].innerText, example2[3].innerText, example2[2].innerText, );
+
+
+const example3 = document.querySelectorAll('li');
+console.log(example3[0].textContent, example3[4].textContent, example3[1].textContent, example3[3].textContent, example3[2].textContent);
